34354258bf0fa829b9645efadf77fc8710b77b13
samynaj <samuelnnj@gmail.com>
2020-09-02 09:49:52 +0100

    added icons and logo to the header component

--- src/home/HeaderBox.js
+++ src/home/HeaderBox.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, Box, Grid, Paper, Button } from '@material-ui/core';
 import SearchBox from './SearchBox';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import HelpOutlineOutlinedIcon from '@material-ui/icons/HelpOutlineOutlined';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    padding: theme.spacing(3),
+    padding: theme.spacing(2),
   },
   paper: {
     padding: theme.spacing(2),
@@ -14,7 +19,14 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
   },
   logo: {
-    width: '70%'
+    width: '70%',
+    height: '1.8rem',
+  },
+  textstyle: {
+    textTransform: 'capitalize',
+    '&:hover': {
+      color: '#3f15b5'
+    }
   }
 }));
 
@@ -25,22 +37,28 @@ export function HeaderBox() {
     <Box className={classes.root}>
       <Container>
         <Grid container spacing={2}>
-          <Grid item xs={12} sm={3}>
-            <img src="/logo.png" className={classes.logo} />
+          <Grid item xs={12} sm={2}>
+            <img src="/img/Gotradelogo.png" className={classes.logo} />
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={6} >
             <SearchBox />
           </Grid>
-          <Grid item xs={12} sm={3}>
-            <Button variant="contained" color="primary" size="large">
-              LOGIN
-          </Button>
-            <Button variant="contained" color="primary" size="large">
-              HELP
-    </Button>
-            <Button variant="contained" color="primary" size="large">
-              CART
-</Button>
+          <Grid item xs={12} sm={4}>
+            <Button className={classes.textstyle}>
+              <PersonOutlineOutlinedIcon fontSize='medium'/>
+              login 
+              <ExpandMoreIcon />
+            </Button>
+            <Button className={classes.textstyle}>
+              <HelpOutlineOutlinedIcon fontSize='medium'/>
+              Help 
+              <ExpandMoreIcon />
+            </Button>
+            <Button className={classes.textstyle}>
+              <ShoppingCartOutlinedIcon fontSize='medium'/>
+              Cart 
+            </Button>    
+                
           </Grid>
         </Grid>
       </Container>
